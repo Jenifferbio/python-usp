c7acc4378f68cd127a79be6bdbd630c0189d9fad
Jeniffer Fonseca <79013581+Jenifferbio@users.noreply.github.com>
2023-01-23 18:09:17 -0300

    Delete exercicio1_opci_lista3.py

--- exercicio1_opci_lista3.py
+++
@@ -1,8 +0,0 @@
-#Escreva um programa que receba um número inteiro positivo na entrada e verifique se é primo. Se o número for primo,
-# imprima "primo". Caso contrário, imprima "não primo".
-
-n = int(input('Digite um numero inteiro'))
-if (n // 2) == 0:
-    print('primo')
-else:
-    print('não primo')
