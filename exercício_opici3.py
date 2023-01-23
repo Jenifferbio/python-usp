627c21e8eb65541e17cf68ebf17e4d233f3df6e6
Jeniffer Fonseca <79013581+Jenifferbio@users.noreply.github.com>
2023-01-23 18:07:13 -0300

    Delete exercício_opici3.py

--- exercício_opici3.py
+++
@@ -1,5 +0,0 @@
-#Faça um programa em Python que recebe um número inteiro e imprime seu dígito das dezenas.
-n = int(input('Digite um número inteiro'))
-print(n)
-d = (n//10) % 10
-print('O digito das dezenas é', d)
