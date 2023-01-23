1750ab876e90b3ea7f7ac05aac0e4adea2ca3594
Jeniffer Fonseca <79013581+Jenifferbio@users.noreply.github.com>
2023-01-23 18:08:44 -0300

    Delete exercicio1_lista3.py

--- exercicio1_lista3.py
+++
@@ -1,9 +0,0 @@
-#Escreva um programa que receba um número natural  n n na entrada e imprima  n! n! (fatorial) na saída.
-
-n = int(input('Digite o valor de n'))
-resultado = 1
-count = 1
-while count <= n:
-    resultado *= count
-    count += 1
-print(resultado)
